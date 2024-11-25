10deb1b440e3a2084b7f103c58591b7fb1bf8a7e
chaeyeon5310 <118064144+chaeyeon5310@users.noreply.github.com>
2024-11-25 19:31:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [],
-  server: {
-    host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
-  }
-})
+    server: {
+        host: '0.0.0.0',
+        hmr: true,
+    }
+});
